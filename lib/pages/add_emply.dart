7a569e14827b324e951b840e798c8effa1af78de
Abubakar Siddique PA <siddiquepanathur@gmail.com>
2023-12-02 11:25:33 +0530

    form validation

--- lib/pages/add_emply.dart
+++ lib/pages/add_emply.dart
@@ -8,6 +8,7 @@ class AddEmployeeDetails extends StatefulWidget {
 }
 
 class _AddEmployeeDetailsState extends State<AddEmployeeDetails> {
+  final _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -21,49 +22,69 @@ class _AddEmployeeDetailsState extends State<AddEmployeeDetails> {
             topRight: Radius.circular(20.0),
           ),
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text("Employee Name"),
-            SizedBox(
-              height: 5,
-            ),
-            TextField(
-              cursorWidth: 5,
-              decoration: InputDecoration(
-                  hintText: "name",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0))),
-              autofocus: true,
-              textAlign: TextAlign.center,
-              onChanged: (value) {},
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Text("Employee Age"),
-            SizedBox(
-              height: 5,
-            ),
-            TextField(
-              cursorWidth: 5,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  hintText: "age",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0))),
-              autofocus: true,
-              textAlign: TextAlign.center,
-              onChanged: (value) {},
-            ),
-            TextButton(
-              onPressed: () {},
-              child: const Text(
-                "Add Employee",
-                style: TextStyle(fontSize: 20, color: Colors.blue),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text("Employee Name"),
+              const SizedBox(
+                height: 5,
               ),
-            ),
-          ],
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter employee name";
+                  }
+                  return null;
+                },
+                cursorWidth: 5,
+                decoration: InputDecoration(
+                    hintText: "name",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+                autofocus: true,
+                textAlign: TextAlign.center,
+                onChanged: (value) {},
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              const Text("Employee Age"),
+              const SizedBox(
+                height: 5,
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter employee age";
+                  }
+                  return null;
+                },
+                cursorWidth: 5,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    hintText: "age",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+                autofocus: true,
+                textAlign: TextAlign.center,
+                onChanged: (value) {},
+              ),
+              TextButton(
+                onPressed: () {
+                  if (_formkey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context)
+                        .showSnackBar(SnackBar(content: Text("processing")));
+                  }
+                },
+                child: const Text(
+                  "Add Employee",
+                  style: TextStyle(fontSize: 20, color: Colors.blue),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
